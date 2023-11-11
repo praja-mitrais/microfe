e24c14b9b7c14075cab40bdc3a1df005487a0c23
I Made Budi Dharma Wipraja <wiprajad@gmail.com>
2023-11-11 15:14:40 +0700

    add employee module

--- hrm-employee/vite.config.ts
+++ hrm-employee/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // We found several issue when importing static assets from remote modules.
+  // To prevent this always change the base to URL your remote hosted
+  base: 'http://localhost:3001/',
+  preview: {
+    port: 3001,
+    strictPort: true,
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "EmployeeModule",
+      filename: "employeeModule.js",
+      exposes: {
+        './App': './src/App.tsx',
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
--- hrm-host/vite.config.ts
+++ hrm-host/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "HRMHost",
+      remotes: {
+        EmployeeModule: "http://localhost:3001/assets/employeeModule.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
